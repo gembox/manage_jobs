30694cff8284ef07507d07514c96c00d7a57f0d9
Ross <rhelenius@gmail.com>
2023-08-16 07:43:28 -0400

    Update kill_long_jobs.py
    reworking logic to use the pager entries directly instead of log files

--- kill_long_jobs.py
+++ kill_long_jobs.py
@@ -7,11 +7,8 @@
 import datetime
 import pytz
 import tableauserverclient as TSC
-import truststore
 import os
 import configparser
-
-truststore.inject_into_ssl()
 
 def calculate_duration_from_utc(start_datetime_utc):
     start_datetime_utc = datetime.datetime.strptime(start_datetime_utc, "%Y-%m-%d %H:%M:%S%z")
@@ -44,71 +41,58 @@ def parse_log_data(log_data):
     return parsed_data
 
 if __name__ == "__main__":
-    log_file_path = "log_file.txt"
+    try: 
+        log_file_path = "log_file.txt"
 
-    #read credentials from the config file
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    name = config.get('tableau', 'name')
-    pat = config.get('tableau', 'pat')
-    site = config.get('tableau', 'site')
-    timeout = config.get('tableau', 'minutes')
-    cloud_server = config.get('tableau', 'server')
+        #read credentials from the config file
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        name = config.get('tableau', 'name')
+        pat = config.get('tableau', 'pat')
+        site = config.get('tableau', 'site')
+        timeout = config.get('tableau', 'minutes')
+        cloud_server = config.get('tableau', 'server')
 
-    tableau_auth = TSC.PersonalAccessTokenAuth(name, pat, site_id=site)
-    server = TSC.Server(cloud_server, use_server_version=True)    
-    server.auth.sign_in(tableau_auth)
+        tableau_auth = TSC.PersonalAccessTokenAuth(name, pat, site_id=site)
+        server = TSC.Server(cloud_server, use_server_version=True)    
+        server.auth.sign_in(tableau_auth)
 
-    with server.auth.sign_in(tableau_auth):
-        req = TSC.RequestOptions()
-        #get jobs that are in progress with a run time greater than 0
-        req.filter.add(TSC.Filter("progress", TSC.RequestOptions.Operator.GreaterThanOrEqual, 0))
-        req.filter.add(TSC.Filter("status", TSC.RequestOptions.Operator.Equals, "InProgress"))
-        target_jobs = (job for job in TSC.Pager(server.jobs, request_opts=req) )
+        with server.auth.sign_in(tableau_auth):
+            req = TSC.RequestOptions()
+            #get jobs that are in progress with a run time greater than 0
+            req.filter.add(TSC.Filter("progress", TSC.RequestOptions.Operator.GreaterThanOrEqual, 0))
+            req.filter.add(TSC.Filter("status", TSC.RequestOptions.Operator.Equals, "InProgress"))
+            target_jobs = (job for job in TSC.Pager(server.jobs, request_opts=req) )
 
-        with open('jobs.txt', 'a') as f:
-            for job in target_jobs: 
-                if job.type == 'run_flow':
-                    f.write(f"{job.type}, {job.name}, {job.id}, {job.status}, {job.started_at}, {job.ended_at}\n")
+            in_progress_entries = [job for job in target_jobs if job.type == 'run_flow' and job.status == 'InProgress']
 
-        try:
-            with open('jobs.txt', "r") as log_file:
-                log_data = log_file.read()
-
-            parsed_data = parse_log_data(log_data)
-
-            in_progress_entries = [entry for entry in parsed_data if entry["status"] == "InProgress"]
-
-            #find the jobs that are in progress
+            #find the jobs that are in progress and parse their attributes
             if in_progress_entries:
                 for entry in in_progress_entries:
-                    print("Action:", entry["action"])
-                    print("Run ID:", entry["run_id"])
-                    print("Status:", entry["status"])
-                    print("Start Time:", entry["start_time"])
-                    
-                    if entry["end_time"] == "None":
-                        print("End Time: Still In Progress")
-                    else:
-                        print("End Time:", entry["end_time"])
+                    print("Action:", entry.type)
+                    print("Run ID:", entry.id)
+                    print("Status:", entry.status)
+                    print("Start Time:", entry.started_at)
 
-                    if entry["status"] == "InProgress":
-                        duration = calculate_duration_from_utc(entry["start_time"])
+                    if entry.status == "InProgress":
+                        duration = calculate_duration_from_utc(str(entry.started_at))
                         print("Duration:", duration)
                         if duration > datetime.timedelta(minutes=int(timeout)):
                             # Cancel the job with the given run ID, if greater than our cogured timeout
-                            server.jobs.cancel(entry["run_id"])
-                            entry["status"] = "Cancelled" #update the status of the job in the parsed data
-                            cancellation_time = datetime.datetime.now(pytz.utc)
-                            entry["end_time"] = cancellation_time.strftime("%Y-%m-%d %H:%M:%S%z")  # Update end time to the time of cancellation
-                
-                            #write the job back to the parsed file
-                            with open('jobs.txt', 'w') as f:
-                                for entry in parsed_data:
-                                    f.write(f"{entry['action']}, {entry['run_id']}, {entry['status']}, {entry['start_time']}, {entry['end_time']}, {duration}\n")
+                            try:
+                                server.jobs.cancel(entry.id)
+                                status = "Cancelled"
+                                cancelled = datetime.datetime.now(pytz.utc)
 
-                        else:
-                            print("No entries with status 'InProgress' and greater than duration found.")
+                                # Append the job information to the log file
+                                with open('jobs.txt', 'a') as f:
+                                    f.write(f"{entry.type}, {entry.id}, {status}, {entry.started_at}, {cancelled}, {duration}\n")
+                            except TSC.ServerResponseError as error:
+                                print(f"Error cancelling job {entry.id}: {error}")
+                        else: 
+                            print("Job has not exceeded timeout.")
+            else:
+                print("No entries with status 'InProgress' found.")
 
-        except FileNotFoundError:
-            print(f"Log file not found at {log_file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
